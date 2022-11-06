ab6e86cd6d36523a6879d5a900f30e13d18cf842
francanete <francanetedev@gmail.com>
2022-11-06 19:02:54 +0000

    fix empty note height bug

--- src/components/EmptyNote.tsx
+++ src/components/EmptyNote.tsx
@@ -1,9 +1,5 @@
-import React from "react";
 import { Link } from "react-router-dom";
-import { Badge } from "./Badge";
 import { Heading } from "./Heading";
-import { INotes } from "../types/notes";
-import DOMPurify from "dompurify";
 import classNames from "classnames";
 import { FaPlus } from "react-icons/fa";
 
