b8627e6666613f3a88add41a6c8b31c7b2fc8aac
francanete <francanetedev@gmail.com>
2023-01-09 18:37:01 +0000

    add new docs

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>StandNotes</span>,
+  logo: <span>StandNotes Docs</span>,
   project: {
     link: "https://github.com/francanete/standnotes",
   },
