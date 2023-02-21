40cf13b9d6c1acdada9eff95ebd86bbed88ee539
francanete <francanetedev@gmail.com>
2023-02-21 18:33:45 +0000

    include uploads forlder

--- apps/server/middleware/uploadFile.ts
+++ apps/server/middleware/uploadFile.ts
@@ -6,7 +6,6 @@ const storage = multer.diskStorage({
     cb(null, "uploads");
   },
   filename: (req, file, cb) => {
-    console.log(file);
     cb(null, "avatar-" + Date.now() + path.extname(file.originalname));
   },
 });
